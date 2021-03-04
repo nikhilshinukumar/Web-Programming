f7c565f5d867d2e7016b0417b536cbad19fcace6
nikhilshinukumar <74667418+nikhilshinukumar@users.noreply.github.com>
2021-03-04 07:13:09 +0530

    Add files via upload

---
+++ Cycle-5/C5.5LOGIN.php
@@ -0,0 +1,25 @@
+<!--Program to check whether the entered username and password are correct-->
+<html>
+<head>
+<title>Login</title>
+</head>
+<body>
+<?php
+// Check if username and password are correct
+if ($_POST["username"] == "php" && $_POST["password"] == "php") {
+// If correct, we set the session to YES
+ session_start();
+ $_SESSION["Login"] = "YES";
+ echo "<h1>You are now logged correctly in</h1>";
+ echo "<p><a href='C5.5document.php'>Link to protected file</a><p/>";
+}
+else {
+// If not correct, we set the session to NO
+ session_start();
+ $_SESSION["Login"] = "NO";
+ echo "<h1>You are NOT logged correctly in </h1>";
+ echo "<p><a href='C5.5document.php'>Link to protected file</a></p>";
+}
+?>
+</body>
+</html>
